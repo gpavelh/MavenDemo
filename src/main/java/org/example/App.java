77b2c12c217e5e293656802f502663587461b3cf
Pavel <gh7777@yandex.ru>
2020-03-04 17:41:26 +0300

    MavenDemo jar 1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,12 +6,9 @@ import org.slf4j.LoggerFactory;
 public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-    public void sayHi(String name) {
-        LOG.info("Hi {}!", name);
-    }
-
-    public static void main(String[] args) {
-        App app = new App();
-        app.sayHi("Петро");
+    public String sayHi(String name) {
+        String str = String.format("Hi, ", name);
+        LOG.info(str);
+        return str;
     }
 }
