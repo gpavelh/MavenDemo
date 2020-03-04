ad11841112e643432823c2a17c23b6b40c587413
Pavel <gh7777@yandex.ru>
2020-03-04 16:52:03 +0300

    MadenDemo 1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public void sayHi(String name) {
+        LOG.info("Hi {}!", name);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.sayHi("Петро");
     }
 }
